f0f855d944bcf63a80079849d0bfce1f56ecc179
callmedron <Andreyfrendak@yandex.ru>
2025-02-09 18:15:41 +0300

    housekeeping

--- src/geometry3D/safeDouble/safeDouble.h
+++ src/geometry3D/safeDouble/safeDouble.h
@@ -11,18 +11,21 @@ public:
   TSafeDouble();
   TSafeDouble(double v);
 
-  TSafeDouble operator+(const TSafeDouble& other) const;
-  TSafeDouble operator-(const TSafeDouble& other) const;
-  TSafeDouble operator-() const;
-
   bool operator>(const TSafeDouble& other) const;
   bool operator<(const TSafeDouble& other) const;
-
-  TSafeDouble abs() const;
 
   bool operator==(const TSafeDouble& other) const;
   bool operator!=(const TSafeDouble& other) const;
 
+  bool operator>=(const TSafeDouble& other) const;
+  bool operator<=(const TSafeDouble& other) const;
+
+  TSafeDouble abs() const;
+
+  TSafeDouble operator-() const;
+
+  TSafeDouble operator+(const TSafeDouble& other) const;
+  TSafeDouble operator-(const TSafeDouble& other) const;
   TSafeDouble operator*(const TSafeDouble& other) const;
   TSafeDouble operator/(const TSafeDouble& other) const;
   TSafeDouble operator^(const TSafeDouble& other) const;
@@ -33,11 +36,8 @@ public:
   TSafeDouble& operator/=(const TSafeDouble& other);
   TSafeDouble& operator^=(const TSafeDouble& other);
 
-  bool operator>=(const TSafeDouble& other) const;
-  bool operator<=(const TSafeDouble& other) const;
-
   friend std::ostream& operator<<(std::ostream& os, const TSafeDouble& sdouble);
-  void print();
+  void print() const;
 };
 
 } // namespace NGeometry3D
