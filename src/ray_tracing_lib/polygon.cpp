a19752a41d22dbb0e9c2f928904aa6c158df78c2
callmedron <Andreyfrendak@yandex.ru>
2025-06-10 15:17:44 +0300

    polygon

--- src/ray_tracing_lib/polygon.cpp
+++ src/ray_tracing_lib/polygon.cpp
@@ -1,5 +1,5 @@
 #include "polygon.h"
-#include "plane.h"
+#include "line.h"
 
 #include <algorithm>
 #include <cmath>
@@ -7,34 +7,62 @@
 
 namespace NRayTracingLib {
 
-// void TPolygon::SortByPolarAngle(const TPlane& plane) {
-//     TVector centerVector;
-//     for (const auto& point : Points) {
-//         centerVector += TVector{point};
-//     }
-//     centerVector /= TSafeDouble{Points.size()};
-//     const TPoint center{centerVector};
+bool TPolygon::IsLine() const {
+    const TLine line{Points[0], Points[1]};
+    for (size_t i = 2; i < Points.size(); i++) {
+        if (!line.containsPoint(Points[i])) {
+            return false;
+        }
+    }
+    return true;
+}
 
-//     TVector xAxis = Points[0] - center;
-//     xAxis.normalize();
-//     TVector yAxis = xAxis ^ plane.Normal;
-//     yAxis.normalize();
+void TPolygon::SortByPolarAngle(const TPlane& plane) {
+    TVector centerVector;
+    for (const auto& point : Points) {
+        centerVector += TVector{point};
+    }
+    centerVector /= static_cast<double>(Points.size());
+    const TPoint center{centerVector};
 
-//     std::sort(Points.begin(), Points.end(), [&center, &xAxis, &yAxis](const TPoint& a, const TPoint& b) {
-//         double aX = (a - center).projectTo(xAxis).length().Value;
-//         double aY = (a - center).projectTo(yAxis).length().Value;
+    TVector xAxis;
+    for (const auto& point : Points) {
+        if (!(point - center).isZero()) {
+            xAxis = point - center;
+            break;
+        }
+    }
+    if (xAxis.isZero()) {
+        throw std::runtime_error("Error: zero xAxis in SortByPolarAngle");
+    }
+    xAxis.normalize();
+    TVector yAxis = xAxis ^ plane.Normal;
+    yAxis.normalize();
 
-//         double bX = (b - center).projectTo(xAxis).length().Value;
-//         double bY = (b - center).projectTo(yAxis).length().Value;
+    std::vector<std::pair<TPoint, double>> pointsWithAngles;
+    pointsWithAngles.reserve(Points.size());
 
-//         return std::atan2(aY, aX) < std::atan2(bY, bX);
-//     });
-// }
+    for (const auto& point : Points) {
+        TVector vec = point - center;
+        double x = (vec * xAxis).Value;
+        double y = (vec * yAxis).Value;
+        double angle = std::atan2(y, x);
+        pointsWithAngles.emplace_back(point, angle);
+    }
 
-// void TPolygon::RemoveExtraPoints() {}
+    std::sort(pointsWithAngles.begin(), pointsWithAngles.end(),
+              [](const auto& lhs, const auto& rhs) { return lhs.second < rhs.second; });
+
+    for (size_t i = 0; i < Points.size(); ++i) {
+        Points[i] = pointsWithAngles[i].first;
+    }
+}
+
+void TPolygon::RemoveExtraPoints() {}
+
+bool TPolygon::IsConvex() const { return true; }
 
 TPolygon::TPolygon(const TPoints& points) {
-    // unique points only
     std::unordered_set<TPoint> uniquePoints;
     for (const auto& point : points) {
         if (!uniquePoints.contains(point)) {
@@ -43,12 +71,10 @@ TPolygon::TPolygon(const TPoints& points) {
         }
     }
 
-    // count of unique points >= 3
     if (Points.size() < 3) {
         throw std::runtime_error("Error: creating polygon by < 3 unique points");
     }
 
-    // all points on one plane
     const TPlane plane{Points[0], Points[1], Points[2]};
     for (size_t i = 3; i < Points.size(); i++) {
         if (!plane.containsPoint(Points[i])) {
@@ -56,17 +82,24 @@ TPolygon::TPolygon(const TPoints& points) {
         }
     }
 
-    // SortByPolarAngle(plane);
+    if (IsLine()) {
+        throw std::runtime_error("Error: creating polygon by points that lie on the same line");
+    }
 
-    // RemoveExtraPoints();
+    SortByPolarAngle(plane);
 
-    // effective count of points >= 3
+    RemoveExtraPoints();
+
     if (Points.size() < 3) {
-        throw std::runtime_error("Error: creating polygon by < 3 unique points");
+        throw std::runtime_error("Error: creating polygon by < 3 effective points");
+    }
+
+    if (!IsConvex()) {
+        throw std::runtime_error("Error: polygon is not convex");
     }
 }
 
-TRectangle::TRectangle(const TPoints& points) : TPolygon(std::move(points)) {
+TRectangle::TRectangle(const TPoints& points) : TPolygon(points) {
     if (Points.size() != 4) {
         throw std::runtime_error("Rectangle must have exactly 4 points");
     }
