3dbfb3cf1e1bcc0d6d2b19bcde6b3791a2cf0c9a
callmedron <Andreyfrendak@yandex.ru>
2025-06-15 03:18:43 +0300

    rays in camera

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,31 @@
 #include "ray_tracing_lib/all.h"
 
+#include <chrono>
+
 using namespace NRayTracingLib;
+
+// void makePicture(const TCamera& camera, const std::vector<TFigure>& figures) {}
 
 int main() {
     try {
-        TCamera camera;
+        auto start = std::chrono::steady_clock::now();
 
-        const TPolyhedron dodecahedron = CreateRegularDodecahedron(TPoint(), 10.0);
-        const TPolyhedron icosahedron = CreateRegularIcosahedron(TPoint(), 10.0);
+        const TCamera camera{
+            TPoint{10.0, 10.0, 10.0},
+            TVector{-10.0, -10.0, -10.0},
+            {TAngle{120.0}, TAngle{90.0}},
+            {1200, 900},
+        };
+
+        auto end = std::chrono::steady_clock::now();
+
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+        print(duration.count());
+
+        // const std::vector<TFigure> figures{createRegularDodecahedron(TPoint(), 1.0)};
+
+        // makePicture(camera, figures);
 
     } catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
--- src/ray_tracing_lib/camera.h
+++ src/ray_tracing_lib/camera.h
@@ -1,13 +1,12 @@
 #pragma once
 
 #include "angle.h"
-#include "point.h"
+#include "line.h"
 
 namespace NRayTracingLib {
 
 class TCamera {
   public:
-    TCamera();
     TCamera(const TPoint& position, const TVector& direction, const std::pair<TAngle, TAngle>& viewAngles,
             const std::pair<uint16_t, uint16_t>& resolution);
 
@@ -15,7 +14,13 @@ class TCamera {
     TPoint Position_;
     TVector Direction_;
     std::pair<TAngle, TAngle> ViewAngles_;
-    std::pair<uint16_t, uint16_t> Resolution_;
+    std::pair<size_t, size_t> Resolution_;
+    std::vector<TRay> Rays_;
+
+    TRay& getRay(size_t i, size_t j);
+    TVector getHalfUpVector() const;
+    TVector getHalfLeftVector() const;
+    void initRays();
 };
 
 } // namespace NRayTracingLib
