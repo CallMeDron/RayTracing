87e034d39734c63d1eb23aad545697652e51a99a
callmedron <Andreyfrendak@yandex.ru>
2025-06-15 03:26:38 +0300

    shared ptr

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,9 @@
 
 using namespace NRayTracingLib;
 
-// void makePicture(const TCamera& camera, const std::vector<TFigure>& figures) {}
+using TFigures = std::vector<std::shared_ptr<TFigure>>;
+
+void makePicture(const TCamera& camera, const TFigures& figures) {}
 
 int main() {
     try {
@@ -17,15 +19,15 @@ int main() {
             {1200, 900},
         };
 
+        const TFigures figures{
+            std::make_shared<TPolyhedron>(createRegularDodecahedron(TPoint{0.0, 0.0, 0.0}, 1.0)),
+        };
+
+        makePicture(camera, figures);
+
         auto end = std::chrono::steady_clock::now();
-
         auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-
         print(duration.count());
-
-        // const std::vector<TFigure> figures{createRegularDodecahedron(TPoint(), 1.0)};
-
-        // makePicture(camera, figures);
 
     } catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
