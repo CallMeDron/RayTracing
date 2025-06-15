baa6c0795dbf105f9077417ceed6f7987d55a91e
callmedron <Andreyfrendak@yandex.ru>
2025-06-15 20:06:22 +0300

    camera rework

--- src/main.cpp
+++ src/main.cpp
@@ -8,24 +8,24 @@ int main() {
     try {
         auto start = std::chrono::steady_clock::now();
 
-        const TCamera camera{
+        TCamera camera{
             TPoint{10.0, 10.0, 10.0},
             TVector{-10.0, -10.0, -10.0},
-            {TAngle{120.0}, TAngle{90.0}},
-            {360, 270},
+            {TAngle{10.0}, TAngle{10.0}},
+            {700, 700},
         };
+
+        const TPolyhedron figure = createRegularDodecahedron(TPoint{0.0, 0.0, 0.0}, 1.0);
+        camera.makePicture(figure);
+
+        const char* filename = "D:/Coding/C++/RayTracing/src/output.png";
+        camera.savePicture(filename);
 
         auto end = std::chrono::steady_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
         print(duration.count());
 
-        camera.makePicture(createRegularDodecahedron(TPoint{0.0, 0.0, 0.0}, 1.0));
-
-        end = std::chrono::steady_clock::now();
-        duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-        print(duration.count());
-
     } catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
+        print(e.what());
     }
 }
--- src/ray_tracing_lib/camera.h
+++ src/ray_tracing_lib/camera.h
@@ -10,21 +10,24 @@ class TCamera {
     TCamera(const TPoint& position, const TVector& direction, const std::pair<TAngle, TAngle>& viewAngles,
             const std::pair<uint16_t, uint16_t>& resolution);
 
-    const TRay& getRayConst(size_t i, size_t j) const;
-
-    void makePicture(const TFigure& figure) const;
+    void makePicture(const TFigure& figure);
+    void savePicture(const char* filename) const;
 
   private:
     TPoint Position_;
     TVector Direction_;
-    std::pair<TAngle, TAngle> ViewAngles_;
-    std::pair<size_t, size_t> Resolution_;
-    std::vector<TRay> Rays_;
+    TAngle WidthViewAngle_;
+    TAngle HeightViewAngle_;
+    size_t WidthResolution_;
+    size_t HeightResolution_;
 
-    TRay& getRay(size_t i, size_t j);
-    TVector getHalfUpVector() const;
-    TVector getHalfLeftVector() const;
-    void initRays();
+    TVector HalfUpVector_;
+    TVector HalfLeftVector_;
+
+    std::vector<uint8_t> ImageData_;
+
+    void initHalfUpVector();
+    void initHalfLeftVector();
 };
 
 } // namespace NRayTracingLib
